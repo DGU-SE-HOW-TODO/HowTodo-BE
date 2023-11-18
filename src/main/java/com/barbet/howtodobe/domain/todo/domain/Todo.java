f654d468463fe67c4b7b12e8d0da18b92ff9b24a
nykoh2001 <nykoh2001@gmail.com>
2023-11-18 17:29:26 +0900

    feat: todo entity

---
+++ src/main/java/com/barbet/howtodobe/domain/todo/domain/Todo.java
@@ -0,0 +1,40 @@
+package com.barbet.howtodobe.domain.todo.domain;
+
+import com.barbet.howtodobe.domain.calendar.domain.Calendar;
+import com.barbet.howtodobe.domain.category.domain.Category;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "TODO")
+@Getter
+@Setter
+public class Todo { // Date 값은 클라이언트로부터 받아야 해서 BaseTimeEntity X
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long todoId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "calender_id")
+    private Calendar calendar;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    @ColumnDefault("false")
+    private boolean isChecked;
+
+    @Column(nullable = false)
+    @ColumnDefault("false")
+    private boolean isDelay;
+}
