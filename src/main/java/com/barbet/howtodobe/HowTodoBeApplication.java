ac5861074a90255a4d1ca33ff3e861add208f8d2
nykoh2001 <nykoh2001@gmail.com>
2023-11-18 15:25:39 +0900

    initial commit

---
+++ src/main/java/com/barbet/howtodobe/HowTodoBeApplication.java
@@ -0,0 +1,13 @@
+package com.barbet.howtodobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowTodoBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HowTodoBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/barbet/howtodobe/HowTodoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbet.howtodobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowTodoBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
