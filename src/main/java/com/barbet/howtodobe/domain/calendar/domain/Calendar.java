66159851875402aa362dd44e15e35ee5a89156fc
nykoh2001 <nykoh2001@gmail.com>
2023-11-18 17:27:18 +0900

    feat: calendar entity

---
+++ src/main/java/com/barbet/howtodobe/domain/calendar/domain/Calendar.java
@@ -0,0 +1,42 @@
+package com.barbet.howtodobe.domain.calendar.domain;
+
+import com.barbet.howtodobe.domain.BaseTimeEntity;
+import com.barbet.howtodobe.domain.member.domain.Member;
+import lombok.*;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "CALENDAR")
+@Getter
+@Setter
+public class Calendar extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long calendarId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(nullable = false)
+    private int successRate; // 0~100
+
+    @Column(nullable = false)
+    private Date date;
+
+    public Calendar(Member member, Date date){
+        calculateSuccessRate();
+        this.member = member;
+        this.date = date;
+    }
+
+    private void calculateSuccessRate(){
+//        this.successRate =
+        // 이슈: 여기서 쿼리가 들어가도 될까 , 아니면 외부 메소드에서 처리
+    }
+
+}
