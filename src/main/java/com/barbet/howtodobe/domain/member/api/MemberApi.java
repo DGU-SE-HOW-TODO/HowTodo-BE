49321f00547c167a2b449424b713a980ffa6eac6
nykoh2001 <nykoh2001@gmail.com>
2023-11-21 13:51:09 +0900

    refact: signin response

--- src/main/java/com/barbet/howtodobe/domain/member/api/MemberApi.java
+++ src/main/java/com/barbet/howtodobe/domain/member/api/MemberApi.java
@@ -4,6 +4,8 @@ import com.barbet.howtodobe.domain.member.application.MemberSignInService;
 import com.barbet.howtodobe.domain.member.domain.Member;
 import com.barbet.howtodobe.domain.member.dto.SignInRequestDTO;
 import com.barbet.howtodobe.domain.member.dto.SignInResponseDTO;
+import com.barbet.howtodobe.global.common.response.ApiStatus;
+import com.barbet.howtodobe.global.common.response.HowTodoStatus;
 import com.barbet.howtodobe.global.common.response.Message;
 import com.barbet.howtodobe.global.util.TokenProvider;
 import lombok.RequiredArgsConstructor;
@@ -20,14 +22,18 @@ public class MemberApi {
     private final MemberSignInService memberSignInService;
     private final TokenProvider tokenProvider;
 
-    @PostMapping("/member/login")
-    public ResponseEntity<SignInResponseDTO> authorize(@RequestBody SignInRequestDTO signInRequestDTO){
+    @PostMapping(value = "/member/login", produces = "application/json")
+    public ResponseEntity<ApiStatus> authorize(@RequestBody SignInRequestDTO signInRequestDTO){
         Member _member = memberSignInService.signIn(signInRequestDTO);
         String accessToken = tokenProvider.createToken(_member, 8640000);
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add("Authorization", "Bearer " + accessToken);
         SignInResponseDTO signInResponseDTO = new SignInResponseDTO("정상적으로 로그인 되었습니다.");
 
-        return new ResponseEntity(signInResponseDTO, httpHeaders, HttpStatus.OK);
+        return new ResponseEntity(Message
+                .builder()
+                .apiStatus(new ApiStatus(HowTodoStatus.OK, "로그인 성공"))
+                .data(signInResponseDTO).build(),
+                httpHeaders, HttpStatus.OK);
     }
 }
