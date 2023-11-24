ef01b9631bc78d30d7abdb00c72584b74ad42cef
Eunse Jang <senuej37@gmail.com>
2023-11-24 14:32:52 +0900

    fix: findByMemberId 반환 값 Optional 처리

--- src/main/java/com/barbet/howtodobe/domain/member/dao/MemberRepository.java
+++ src/main/java/com/barbet/howtodobe/domain/member/dao/MemberRepository.java
@@ -6,10 +6,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Member findByMemberId(Long memberId);
+
+    // TODO 확인 후 삭제 바람..
+    // 일단 NullPointerException를 방지용으로 Optional 처리해뒀어!
+    // findByEmail도 Optional로 하면 좋을거 같습니당 (관련 로직 괜히 수정하다가 오류 날수도 있으니까 있단 나뒀어..!)
+    // Optional로 묶고 exception 처리는 따로 global에 생성해둔 곳에서 custom 해서 쓰면 좋을거 같아
+    Optional<Member> findByMemberId(Long memberId);
     Member findByEmail(String email);
     List<Member> findAll();
 }
