c921f2ef9c4ff239f52e214539f90f32b0432b85
Eunse Jang <senuej37@gmail.com>
2023-11-24 18:49:35 +0900

    fix: 반환 값 Optional 처리

--- src/main/java/com/barbet/howtodobe/domain/member/dao/MemberRepository.java
+++ src/main/java/com/barbet/howtodobe/domain/member/dao/MemberRepository.java
@@ -11,8 +11,8 @@ import java.util.Optional;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    // TODO 확인 후 삭제 바람..
-    // 일단 NullPointerException를 방지용으로 Optional 처리해뒀어!
+    // TODO 읽었으면 주석 삭제해줘!
+    // NullPointerException를 방지용으로 Optional 처리해뒀어!
     // findByEmail도 Optional로 하면 좋을거 같습니당 (관련 로직 괜히 수정하다가 오류 날수도 있으니까 있단 나뒀어..!)
     // Optional로 묶고 exception 처리는 따로 global에 생성해둔 곳에서 custom 해서 쓰면 좋을거 같아
     Optional<Member> findByMemberId(Long memberId);
