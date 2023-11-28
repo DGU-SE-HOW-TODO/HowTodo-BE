b21e350484cf82fa6e34b1924e1fb4aad50d2ebf
Eunse Jang <senuej37@gmail.com>
2023-11-28 22:42:46 +0900

    remove: 임시 파일 제거

--- src/main/java/com/barbet/howtodobe/domain/statistics/dao/StatisticsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.barbet.howtodobe.domain.statistics.dao;
-
-import com.barbet.howtodobe.domain.statistics.domain.Statistics;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StatisticsRepository extends JpaRepository<Statistics, Long> {
-
-}
