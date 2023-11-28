c39a657edda1fc8eda188bbc8b72d11c5df2d5c4
Eunse Jang <senuej37@gmail.com>
2023-11-28 11:48:56 +0900

    feat: 통계에 사용될 대분류 이름 목록 추출

--- src/main/java/com/barbet/howtodobe/domain/category/dao/CategoryRepository.java
+++ src/main/java/com/barbet/howtodobe/domain/category/dao/CategoryRepository.java
@@ -2,9 +2,26 @@ package com.barbet.howtodobe.domain.category.dao;
 
 import com.barbet.howtodobe.domain.category.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    /** 통계용 */
+
+    // 한 주에 해당하는 대분류 이름 목록 반환 (중복 제거)
+    @Query("SELECT DISTINCT c.name FROM Category c " +
+            "JOIN FETCH c.member m " +
+            "WHERE YEAR(c.createdDate) = :year " +
+            "AND MONTH(c.createdDate) = :month " +
+            "AND c.week = :week")
+    List<String> findCategoryNamesByDate(@Param("year") Integer year,
+                                         @Param("month") Integer month,
+                                         @Param("week") Integer week);
+
 
 }
