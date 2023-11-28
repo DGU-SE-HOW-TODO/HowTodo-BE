245c24856cfc8127ab27296926397ad04b45a49a
Eunse Jang <senuej37@gmail.com>
2023-11-28 22:41:10 +0900

    fix: 통계 Entity 수정

--- src/main/java/com/barbet/howtodobe/domain/statistic/domain/Statistic.java
+++ src/main/java/com/barbet/howtodobe/domain/statistic/domain/Statistic.java
@@ -22,34 +22,20 @@ public class Statistic {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Integer year;
-    private Integer month;
-    private Integer week;
-
-    // private LocalDate selectedDate;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", nullable = false)
     private Member member;
 
-
-    // TODO 일단 하나에 작성하고 나중에 리팩토링
-    @OneToMany(mappedBy = "statistics", cascade = CascadeType.ALL)
-    private List<Todo> todoList = new ArrayList<>();
-    @OneToMany(mappedBy = "statistics", cascade = CascadeType.ALL)
-    private List<Category> categoryList = new ArrayList<>();
-    @OneToMany(mappedBy = "statistics", cascade = CascadeType.ALL)
-    private List<Failtag> failtagList = new ArrayList<>();
-
-
-    /** 투두 통계 */
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "todo_id", nullable = false)
-    private Todo todo;
+    private Integer year;
+    private Integer month;
+    private Integer week;
 
     private Integer prevTodoCnt;
     private Integer prevTodoDoneCnt;
     private Integer nowTodoCnt;
     private Integer nowTodoDoneCnt;
     private Integer rateOfChange;
+
+    private String nowBestCategory;
+    private String nowWorstFailtag;
 }
