feddc08914344628d992a0ee87248d3c39829d9e
Eunse Jang <senuej37@gmail.com>
2023-11-28 22:39:38 +0900

    feat: 이번주 대분류 통계 Entity

---
+++ src/main/java/com/barbet/howtodobe/domain/nowCategory/domain/NowCategory.java
@@ -0,0 +1,31 @@
+package com.barbet.howtodobe.domain.nowCategory.domain;
+
+import com.barbet.howtodobe.domain.statistic.domain.Statistic;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class NowCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "statistic_id", nullable = false)
+    private Statistic statistic;
+
+    private String nowCategory;
+    private Integer nowCategoryRate;
+
+    public NowCategory(String nowCategory, Integer nowCategoryRate) {
+        this.id = id;
+        this.nowCategory = nowCategory;
+        this.nowCategoryRate = nowCategoryRate;
+    }
+}
