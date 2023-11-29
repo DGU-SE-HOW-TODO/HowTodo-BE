7252451e0a56cb04a96e133736226009f8b501a1
Eunse Jang <senuej37@gmail.com>
2023-11-29 21:56:54 +0900

    feat: 이번주 실패태그 Entity

--- src/main/java/com/barbet/howtodobe/domain/nowFailtag/domain/NowFailtag.java
+++ src/main/java/com/barbet/howtodobe/domain/nowFailtag/domain/NowFailtag.java
@@ -1,4 +1,32 @@
 package com.barbet.howtodobe.domain.nowFailtag.domain;
 
+import com.barbet.howtodobe.domain.statistic.domain.Statistic;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class NowFailtag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "statistic_id", nullable = false)
+    private Statistic statistic;
+
+    private String nowFailtag;
+    private Integer nowFailtagRate;
+
+    public NowFailtag(String nowFailtag, Integer nowFailtagRate) {
+        this.id = id;
+        this.nowFailtag = nowFailtag;
+        this.nowFailtagRate = nowFailtagRate;
+    }
+
 }
