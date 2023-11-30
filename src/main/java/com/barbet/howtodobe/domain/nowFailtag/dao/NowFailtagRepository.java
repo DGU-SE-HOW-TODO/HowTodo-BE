b69f790e1abe0f8f8885624b7934880aaa2eb3cd
Eunse Jang <senuej37@gmail.com>
2023-12-01 01:51:14 +0900

    feat: 통계용 실패태그 쿼리문 추가

--- src/main/java/com/barbet/howtodobe/domain/nowFailtag/dao/NowFailtagRepository.java
+++ src/main/java/com/barbet/howtodobe/domain/nowFailtag/dao/NowFailtagRepository.java
@@ -1,4 +1,16 @@
 package com.barbet.howtodobe.domain.nowFailtag.dao;
 
-public interface NowFailtagRepository {
+import com.barbet.howtodobe.domain.category.domain.Category;
+import com.barbet.howtodobe.domain.nowCategory.domain.NowCategory;
+import com.barbet.howtodobe.domain.nowFailtag.domain.NowFailtag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NowFailtagRepository extends JpaRepository<NowFailtag, Long> {
+
+    // nowFailtagRate 오름차순 정렬
+    List<NowFailtag> findAllByOrderByNowFailtagRateAsc();
 }
