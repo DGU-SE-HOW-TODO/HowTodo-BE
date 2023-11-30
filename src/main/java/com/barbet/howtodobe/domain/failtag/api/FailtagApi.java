e2e557fa55fe795f52c6d1c99f872b8a8f90e906
Eunse Jang <senuej37@gmail.com>
2023-11-30 22:28:35 +0900

    fix: 나만의 실패태그 선택 (5개) API

--- src/main/java/com/barbet/howtodobe/domain/failtag/api/FailtagApi.java
+++ src/main/java/com/barbet/howtodobe/domain/failtag/api/FailtagApi.java
@@ -1,4 +1,24 @@
 package com.barbet.howtodobe.domain.failtag.api;
 
+import com.barbet.howtodobe.domain.failtag.application.FailtagService;
+import com.barbet.howtodobe.domain.failtag.dto.FailtagRequestDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/failtag")
 public class FailtagApi {
+
+    private final FailtagService failtagService;
+
+    @PostMapping("/todoCategory")
+    public ResponseEntity<Void> select5Failtag(@RequestBody FailtagRequestDTO request) {
+        failtagService.select5Failtags(request);
+        return ResponseEntity.ok().build();
+    }
 }
