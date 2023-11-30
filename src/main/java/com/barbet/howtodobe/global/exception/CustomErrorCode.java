bcb28f9aceac66c2d50290aeaf956c9222fe2fa7
Eunse Jang <senuej37@gmail.com>
2023-12-01 01:50:05 +0900

    feat: 투두, 실패태그 custom error message

--- src/main/java/com/barbet/howtodobe/global/exception/CustomErrorCode.java
+++ src/main/java/com/barbet/howtodobe/global/exception/CustomErrorCode.java
@@ -18,9 +18,12 @@ public enum CustomErrorCode {
     // Category (3xxx)
 
     // To Do (4xxx)
+    TODO_NOT_FOUND(HttpStatus.NOT_FOUND, 4001, "해당하는 To do가 없습니다."),
 
     // Failtag (5xxx)
-    FAILTAG_COUNT_IS_NOT_FIVE(HttpStatus.BAD_REQUEST, 5001, "선택된 실패태그는 5개이여야 합니다.");
+    FAILTAG_COUNT_IS_NOT_FIVE(HttpStatus.BAD_REQUEST, 5001, "선택된 실패태그는 5개이여야 합니다."),
+    NOT_EXIST_WEEK_FAILTAG(HttpStatus.BAD_REQUEST, 5002, "이번주에 등록된 실패태그가 없습니다."),
+    INVALID_FAILTAG(HttpStatus.BAD_REQUEST, 5003, "잘못 선택된 실패태그 입니다. 이번주에 등록된 실패태그만 선택해주세요");
 
     // Statistics / Feedback (6xxx)
 
