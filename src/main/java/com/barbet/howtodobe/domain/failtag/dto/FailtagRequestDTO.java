bb27dfe5df5adb100b1cde2cc3ec646df3964555
Eunse Jang <senuej37@gmail.com>
2023-11-30 22:29:01 +0900

    feat: 실패태그 request DTO

--- src/main/java/com/barbet/howtodobe/domain/failtag/dto/FailtagRequestDTO.java
+++ src/main/java/com/barbet/howtodobe/domain/failtag/dto/FailtagRequestDTO.java
@@ -1,4 +1,26 @@
 package com.barbet.howtodobe.domain.failtag.dto;
 
+import com.barbet.howtodobe.domain.failtag.domain.AllFailtag;
+import com.barbet.howtodobe.domain.failtag.domain.Failtag;
+import com.barbet.howtodobe.domain.member.domain.Member;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
 public class FailtagRequestDTO {
+
+    private Integer year;
+    private Integer month;
+    private Integer week;
+    private List<String> selectedFailtagList;
+    public Failtag toEntity (Member member) {
+        return Failtag.builder()
+                .member(member)
+                .year(year)
+                .month(month)
+                .week(week)
+                .selectedFailtagList(selectedFailtagList)
+                .build();
+    }
 }
