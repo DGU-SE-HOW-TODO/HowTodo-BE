274538b19dbc4f3e140d343c1953c5025459663c
Eunse Jang <senuej37@gmail.com>
2023-11-30 17:27:30 +0900

    feat: 홈화면 조회 API

---
+++ src/main/java/com/barbet/howtodobe/domain/home/api/HomeApi.java
@@ -0,0 +1,39 @@
+package com.barbet.howtodobe.domain.home.api;
+
+import com.barbet.howtodobe.domain.home.application.HomeService;
+import com.barbet.howtodobe.domain.home.dto.HomeResponseDTO;
+import com.barbet.howtodobe.domain.statistic.application.StatisticService;
+import com.barbet.howtodobe.domain.statistic.dto.StatisticResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDate;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/home")
+public class HomeApi {
+
+    private final HomeService homeService;
+
+    @GetMapping("/home/{selectedDate}")
+    public ResponseEntity<HomeResponseDTO> getHomeInfo(
+            @PathVariable(value = "selectedDate", required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate selectedDate,
+            HttpServletRequest httpServletRequest) {
+
+        if (selectedDate == null) {
+            // selectedDate를 선택하지 않은 경우 현재 날짜로 설정
+            selectedDate = LocalDate.now();
+        }
+        return ResponseEntity.ok().body(homeService.getHomeInfo(selectedDate, httpServletRequest));
+    }
+
+
+}
