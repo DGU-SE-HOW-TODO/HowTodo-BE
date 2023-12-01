d8b505cc47364ac5678a22643b153524fdbccd80
Eunse Jang <senuej37@gmail.com>
2023-12-01 14:41:15 +0900

    setting: 통계 Repository 셋팅

--- src/main/java/com/barbet/howtodobe/domain/statistic/dao/StatisticRepository.java
+++ src/main/java/com/barbet/howtodobe/domain/statistic/dao/StatisticRepository.java
@@ -1,8 +1,14 @@
 package com.barbet.howtodobe.domain.statistic.dao;
 
+import com.barbet.howtodobe.domain.category.domain.Category;
 import com.barbet.howtodobe.domain.statistic.domain.Statistic;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface StatisticRepository extends JpaRepository<Statistic, Long> {
-
 }
