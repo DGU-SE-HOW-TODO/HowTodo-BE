4354770883f97c734f91dd07a7da4dd01623dcc6
Eunse Jang <senuej37@gmail.com>
2023-12-01 14:43:36 +0900

    feat: 나만의 실패태그 (5개) 조회

--- src/main/java/com/barbet/howtodobe/domain/failtag/api/FailtagApi.java
+++ src/main/java/com/barbet/howtodobe/domain/failtag/api/FailtagApi.java
@@ -2,12 +2,19 @@ package com.barbet.howtodobe.domain.failtag.api;
 
 import com.barbet.howtodobe.domain.failtag.application.FailtagService;
 import com.barbet.howtodobe.domain.failtag.dto.FailtagRequestDTO;
+import com.barbet.howtodobe.domain.feedback.dto.FeedbackResponseDTO;
+import com.barbet.howtodobe.domain.todo.application.TodoWithFailtagService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDate;
+import java.time.temporal.TemporalField;
+import java.time.temporal.WeekFields;
+import java.util.List;
+import java.util.Locale;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,10 +22,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class FailtagApi {
 
     private final FailtagService failtagService;
+    private final TodoWithFailtagService todoWithFailtagService;
 
     @PostMapping("/todoCategory")
     public ResponseEntity<Void> select5Failtag(@RequestBody FailtagRequestDTO request) {
         failtagService.select5Failtags(request);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{selectedDate}")
+    public ResponseEntity<List<String>> getSelected5Failtag(@PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate selectedDate) {
+        TemporalField woy = WeekFields.of(Locale.getDefault()).weekOfWeekBasedYear();
+        Integer year = selectedDate.getYear();
+        Integer month = selectedDate.getMonthValue();
+        Integer week = selectedDate.get(woy);
+        return ResponseEntity.ok().body(todoWithFailtagService.findFailtagsBySelectedDate(year, month, week));
     }
 }
