6980314a80cddaa379721e2bf483a9b2eab32d63
Eunse Jang <senuej37@gmail.com>
2023-12-01 14:35:43 +0900

    fix: 대분류 API url 수정

--- src/main/java/com/barbet/howtodobe/domain/category/api/CategoryApi.java
+++ src/main/java/com/barbet/howtodobe/domain/category/api/CategoryApi.java
@@ -9,13 +9,12 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/home")
 public class CategoryApi {
 
     private final CategoryService categoryService;
 
     /** 대분류 등록 */
-    @PostMapping("/todoCategory")
+    @PostMapping("/category")
     public ResponseEntity<Void> createCategory(@RequestBody CategoryRequestDTO request) {
         categoryService.createCategory(request);
         return ResponseEntity.ok().build();
