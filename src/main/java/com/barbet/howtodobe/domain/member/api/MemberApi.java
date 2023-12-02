2160436577478ccab860432f81ea0ce268d4721a
Eunse Jang <senuej37@gmail.com>
2023-12-03 00:17:30 +0900

    feat: 이메일 확인 API

--- src/main/java/com/barbet/howtodobe/domain/member/api/MemberApi.java
+++ src/main/java/com/barbet/howtodobe/domain/member/api/MemberApi.java
@@ -1,21 +1,24 @@
 package com.barbet.howtodobe.domain.member.api;
 
+import com.barbet.howtodobe.domain.member.application.MemberEmailDuplicateService;
 import com.barbet.howtodobe.domain.member.application.MemberSignInService;
 import com.barbet.howtodobe.domain.member.application.MemberSingUpService;
 import com.barbet.howtodobe.domain.member.domain.Member;
 import com.barbet.howtodobe.domain.member.dto.SignInRequestDTO;
 import com.barbet.howtodobe.domain.member.dto.SignInResponseDTO;
 import com.barbet.howtodobe.domain.member.dto.SignUpRequestDTO;
+import com.barbet.howtodobe.global.common.response.ApiStatus;
+import com.barbet.howtodobe.global.common.response.HowTodoStatus;
 import com.barbet.howtodobe.global.common.response.Message;
+import com.barbet.howtodobe.global.exception.CustomException;
 import com.barbet.howtodobe.global.util.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static com.barbet.howtodobe.global.exception.CustomErrorCode.EMAIL_ALREADY_EXIST;
 
 @RestController
 @RequestMapping("/member")
@@ -25,6 +28,8 @@ public class MemberApi {
     private final TokenProvider tokenProvider;
 
     private final MemberSingUpService singUpService;
+    private final MemberEmailDuplicateService memberEmailDuplicateService;
+
 
     @PostMapping("/login")
     public ResponseEntity<String> authorize(@RequestBody SignInRequestDTO signInRequestDTO){
@@ -34,11 +39,51 @@ public class MemberApi {
         httpHeaders.add("Authorization", "Bearer " + accessToken);
         SignInResponseDTO signInResponseDTO = new SignInResponseDTO("정상적으로 로그인 되었습니다.");
 
-        return ResponseEntity.ok().body(accessToken);
-        //return new ResponseEntity(signInResponseDTO, httpHeaders, HttpStatus.OK);
+        return new ResponseEntity(signInResponseDTO, httpHeaders, HttpStatus.OK);
     }
 
-    @PostMapping("/signup")
+    @GetMapping(value = "/{email}", produces = "application/json")
+    public ResponseEntity<ApiStatus> emailDuplicate(@PathVariable("email") String email){
+        HttpHeaders httpHeaders = new HttpHeaders();
+
+        if (memberEmailDuplicateService.EmailDuplicateCheck(email)) {
+            throw  new CustomException(EMAIL_ALREADY_EXIST);
+        }
+
+        try {
+            boolean isDuplicate = memberEmailDuplicateService.EmailDuplicateCheck(email);
+            if (isDuplicate) {
+                return new ResponseEntity(
+                        new ApiStatus(HowTodoStatus.DUPLICATE_EMAIL, "중복된 이메일"),
+                        httpHeaders, HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+            return new ResponseEntity(
+                    new ApiStatus(HowTodoStatus.OK, "이메일 중복 X"),
+                    httpHeaders, HttpStatus.OK);
+        }
+        catch (RuntimeException e){
+            return new ResponseEntity(
+                    new ApiStatus(HowTodoStatus.INTERNEL_SERVER_ERROR, e.getMessage()),
+                    httpHeaders, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+//    @GetMapping(value = "/member/{email}", produces = "application/json")
+//    public ResponseEntity<ApiStatus> emailDuplicate(@PathVariable("email") String email) {
+//        HttpHeaders httpHeaders = new HttpHeaders();
+//        boolean isDuplicate = memberEmailDuplicateService.EmailDuplicateCheck(email);
+//        if (isDuplicate) {
+//            return new ResponseEntity(
+//                    new ApiStatus(HowTodoStatus.DUPLICATE_EMAIL, "중복된 이메일"),
+//                    httpHeaders, HttpStatus.INTERNAL_SERVER_ERROR);
+//        }
+//        return new ResponseEntity(
+//                new ApiStatus(HowTodoStatus.OK, "이메일 중복 X"),
+//                httpHeaders, HttpStatus.OK);
+//    }
+
+
+        @PostMapping("/signup")
     public ResponseEntity<Void> signUp(@RequestBody SignUpRequestDTO request) {
         singUpService.singUp(request);
         return ResponseEntity.ok().build();
