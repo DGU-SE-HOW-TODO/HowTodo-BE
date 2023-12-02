5d25a6e95815a14df5727c287a375f1e11399448
nykoh2001 <nykoh2001@gmail.com>
2023-12-03 01:24:11 +0900

    fix: failtag 1년 기준 주로 저장

--- src/main/java/com/barbet/howtodobe/domain/failtag/application/FailtagService.java
+++ src/main/java/com/barbet/howtodobe/domain/failtag/application/FailtagService.java
@@ -12,7 +12,11 @@ import com.barbet.howtodobe.global.util.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.WeekFields;
 import java.util.List;
+import java.util.Locale;
 
 import static com.barbet.howtodobe.global.exception.CustomErrorCode.FAILTAG_COUNT_IS_NOT_FIVE;
 import static com.barbet.howtodobe.global.exception.CustomErrorCode.USER_NOT_FOUND;
@@ -34,16 +38,27 @@ public class FailtagService {
 
         List<String> selectedFailtagList = request.getSelectedFailtagList();
 
-        if (selectedFailtagList.size() != 5) { // 실패태그 선택 5개해야 함
+        if (selectedFailtagList.size() > 5) { // 실패태그 선택 5개해야 함
             throw new CustomException(FAILTAG_COUNT_IS_NOT_FIVE);
         }
 
+//         FK 제약으로 지울 수 없음
+//        failtagRepository.deleteFailTagByMemberId(Long.parseLong("-1"));
+
         for (String failtagName : selectedFailtagList) {
+            String selectedDate = request.getSelectedDate();
+            LocalDate date = LocalDate.parse(
+                    selectedDate, DateTimeFormatter.ISO_DATE);
+            Integer woy = date.get(WeekFields.of(Locale.getDefault()).weekOfWeekBasedYear());
+            String[] datechunks = selectedDate.split("-");
+            int year= Integer.parseInt(datechunks[0]);
+            int month = Integer.parseInt(datechunks[1]);
+            int week = woy;
             Failtag failtag = Failtag.builder()
                     .member(tempMember)
-                    .year(request.getYear())
-                    .month(request.getMonth())
-                    .week(request.getWeek())
+                    .year(year)
+                    .month(month)
+                    .week(week)
                     .name(failtagName)
                     .selectedFailtagList(List.of(failtagName))
                     .build();
