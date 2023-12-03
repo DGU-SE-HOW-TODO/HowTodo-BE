1a80fe4a7094622697ec3e3e5f6635e0e82d9cc7
Eunse Jang <senuej37@gmail.com>
2023-12-03 13:04:01 +0900

    feat: custom error message 추가

--- src/main/java/com/barbet/howtodobe/global/exception/CustomErrorCode.java
+++ src/main/java/com/barbet/howtodobe/global/exception/CustomErrorCode.java
@@ -16,8 +16,13 @@ public enum CustomErrorCode {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, 2001, "사용자를 찾을 수 없습니다."),
     EMAIL_ALREADY_EXIST(HttpStatus.NOT_FOUND, 2002, "이미 존재하는 이메일입니다."),
     NOT_EXIST_PASSWORD(HttpStatus.NOT_FOUND, 2003, "비밀번호가 입력되지 않았습니다"),
+    TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, 2004, "토큰이 만료되었습니다."),
+    INVALID_TOKEN_FORMAT(HttpStatus.BAD_REQUEST, 2005, "잘못된 토큰 형식입니다."),
+    TOKEN_VALIDATION_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 2006, "토큰 검증 중 오류가 발생했습니다."),
+    LOGIN_FAILED(HttpStatus.UNAUTHORIZED, 2007, "잘못된 비밀번호입니다."),
 
     // Category (3xxx)
+    NOT_EXIST_WEEK_CATEGORY(HttpStatus.BAD_REQUEST, 3001, "이번주에 관한 카테고리가 없습니다."),
 
     // To Do (4xxx)
     TODO_NOT_FOUND(HttpStatus.NOT_FOUND, 4001, "해당하는 To do가 없습니다."),
