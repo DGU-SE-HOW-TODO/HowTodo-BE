6a0d75420b7531c29f15458905056ecc9fb13392
Eunse Jang <senuej37@gmail.com>
2023-12-03 21:39:22 +0900

    fix: 대분류명으로 대분류 id값 찾기

--- src/main/java/com/barbet/howtodobe/domain/category/dao/CategoryRepository.java
+++ src/main/java/com/barbet/howtodobe/domain/category/dao/CategoryRepository.java
@@ -1,11 +1,13 @@
 package com.barbet.howtodobe.domain.category.dao;
 
 import com.barbet.howtodobe.domain.category.domain.Category;
+import com.barbet.howtodobe.domain.failtag.domain.Failtag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,4 +16,10 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     // 대분류 ID로 대분류명 찾기
     Category findCategoryByCategoryId(Long categoryId);
+
+    @Query("SELECT c.categoryId FROM Category c "+
+            "WHERE c.name = :categoryName")
+    Optional<Long> findByCategoryName(@Param("categoryName") String categoryName);
+
+
 }
