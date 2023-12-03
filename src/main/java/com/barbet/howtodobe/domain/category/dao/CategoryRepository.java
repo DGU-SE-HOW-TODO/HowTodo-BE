c03e0404bcbc81d4d17bf6c9747dd347f1a397ac
Eunse Jang <senuej37@gmail.com>
2023-12-03 13:01:25 +0900

    fix: 필요없는 쿼리 삭제

--- src/main/java/com/barbet/howtodobe/domain/category/dao/CategoryRepository.java
+++ src/main/java/com/barbet/howtodobe/domain/category/dao/CategoryRepository.java
@@ -7,20 +7,10 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-    /** 통계용 */
-    // 한 주에 해당하는 대분류 id 목록 반환 (중복 제거)
-    @Query("SELECT c.categoryId FROM Category c " +
-            // "JOIN FETCH c.member m " +
-            "WHERE YEAR(c.createdDate) = :year " +
-            "AND MONTH(c.createdDate) = :month " +
-            "AND c.week = :week")
-    List<Long> findCategoryIdsByDate(@Param("year") Integer year,
-                                     @Param("month") Integer month,
-                                     @Param("week") Integer week);
 
     // 대분류 ID로 대분류명 찾기
     Category findCategoryByCategoryId(Long categoryId);
