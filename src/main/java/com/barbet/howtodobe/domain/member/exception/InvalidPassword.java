c88fd5ed6be8df6fe098205a20e133997905fac7
Eunse Jang <senuej37@gmail.com>
2023-12-07 22:33:33 +0900

    remove: 불필요한 파일 삭제

--- src/main/java/com/barbet/howtodobe/domain/member/exception/InvalidPassword.java
+++
@@ -1,5 +0,0 @@
-package com.barbet.howtodobe.domain.member.exception;
-
-public class InvalidPassword extends RuntimeException{
-    private final String errorMessage = "비밀번호에 해당하는 멤버가 존재하지 않습니다.";
-}
