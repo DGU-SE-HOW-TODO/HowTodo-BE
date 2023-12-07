d5295863661974af1c16ce4fd5ae459e2a5ae0d6
Eunse Jang <senuej37@gmail.com>
2023-12-07 22:41:57 +0900

    style: 코드 정리

--- src/main/java/com/barbet/howtodobe/domain/todo/domain/Priority.java
+++ src/main/java/com/barbet/howtodobe/domain/todo/domain/Priority.java
@@ -8,8 +8,8 @@ import lombok.Getter;
 public enum Priority {
     VERY_IMPORTANT(1,"매우 중요"),
     IMPORTANT(2,"중요"),
-    NOT_IMPORTANT(3,"중요하지 않음"),
-    ;
+    NOT_IMPORTANT(3,"중요하지 않음");
+
     private int index;
     private String priority;
 }
